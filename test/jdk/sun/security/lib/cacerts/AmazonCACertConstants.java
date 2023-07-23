f4db6d0a1397b39d90b5ac24248402211ec621c8
Sergey Bylokhov <bylokhov@amazon.com>
2023-07-22 21:20:03 -0700

    Fix Amazon copyright in various files

--- test/jdk/sun/security/lib/cacerts/AmazonCACertConstants.java
+++ test/jdk/sun/security/lib/cacerts/AmazonCACertConstants.java
@@ -1,16 +1,14 @@
 /*
- * Copyright (c) 2023, Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation. Amazon designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
  *
